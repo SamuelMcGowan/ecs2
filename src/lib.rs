85c921a481840e586b2b818a33aa05f4a074b3bc
Sam McGowan <the.sam.mcg@protonmail.com>
2023-02-13 17:06:46 +0000

    Prelude.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
-pub mod sparse;
-pub mod storage;
 mod erased_storages;
+mod sparse;
+mod storage;
+
+pub mod prelude {
+    pub use crate::storage::components::Component;
+    pub use crate::storage::entities::{EntityError, EntityId, EntityIter};
+    pub use crate::storage::unique::Unique;
+}
