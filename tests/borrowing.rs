1429a989bdf2b8a0a1e82b3d100a168a6c0c01ef
Sam McGowan <the.sam.mcg@protonmail.com>
2023-02-14 17:29:47 +0000

    Test uniques.

--- tests/borrowing.rs
+++ tests/borrowing.rs
@@ -5,8 +5,11 @@ use ecs2::query::QueryError;
 struct MyCmp(usize);
 impl Component for MyCmp {}
 
+struct MyUnique(String);
+impl Unique for MyUnique {}
+
 #[test]
-fn add_and_borrow() {
+fn add_and_borrow_comp() {
     let mut world = World::new();
 
     let entity = world.spawn().unwrap();
@@ -24,4 +27,20 @@ fn add_and_borrow() {
 
     let my_cmps = world.borrow::<QueryComp<MyCmp>>().unwrap();
     assert!(matches!(my_cmps.get(entity), Ok(MyCmp(12))));
+}
+
+#[test]
+fn add_and_borrow_unique() {
+    let mut world = World::new();
+
+    world.insert_unique(MyUnique("hello, world!".to_owned()));
+
+    let mut my_unique_mut = world.borrow::<QueryUniqueMut<MyUnique>>().unwrap();
+    my_unique_mut.get_mut().0.push_str(" how are you?");
+
+    assert!(matches!(world.borrow::<QueryUnique<MyUnique>>(), Err(QueryError::BorrowError(_))));
+    drop(my_unique_mut);
+
+    let my_unique = world.borrow::<QueryUnique<MyUnique>>().unwrap();
+    assert!(matches!(my_unique.get().0.as_str(), "hello, world! how are you?"));
 }
